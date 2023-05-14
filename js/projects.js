136dbefb1ca11b1bd386c54a3eb58c0083a21799
Benoît Pntier <46506005+benpntier@users.noreply.github.com>
2023-05-15 00:10:29 +0200

    JavaScript-generated project list

--- js/projects.js
+++ js/projects.js
@@ -2,7 +2,132 @@ let projectsData = [];
 
 function addProjectHTML(project) {
     if (project.category == "pro") {
-        const projectName = document.createElement("h4");
+        const proLogo = document.createElement("img");
+        proLogo.src = project.logo_url;
+        proLogo.alt = project.logo_alt;
+        proLogo.className = "pro__img";
+
+        const proName = document.createElement("h4");
+        proName.className = "no-margin";
+        proName.innerHTML = "<b>" + project.name + "</b>, " + project.company;
+
+        const proDates = document.createElement("p");
+        proDates.classList.add("projects__dates", "no-margin");
+        proDates.innerText = project.dates
+
+        const proInfo = document.createElement("div");
+        proInfo.className = "projects__info";
+        proInfo.appendChild(proName);
+        proInfo.appendChild(proDates);
+
+        const proHeader = document.createElement("div");
+        proHeader.className = "pro__header";
+        proHeader.appendChild(proLogo);
+        proHeader.appendChild(proInfo);
+
+        const proDesc = document.createElement("p");
+        proDesc.classList.add("pro__desc", "no-margin");
+        proDesc.innerHTML = project.desc;
+
+        const proTags = document.createElement("div");
+        proTags.className = "tags";
+        for (var t=0; t < project.tags_short.length; t++) {
+            var tag = document.createElement("span");
+            tag.className = project.tags_colors_short[t];
+            tag.innerText = project.tags_short[t];
+            proTags.appendChild(tag);
+        }
+
+        const proArticle = document.createElement("article");
+        proArticle.classList.add("projects__card", "pro__project");
+        proArticle.appendChild(proHeader);
+        proArticle.appendChild(proDesc);
+        proArticle.appendChild(proTags);
+
+        const proLink = document.createElement("a");
+        proLink.href = "project.html";
+        proLink.appendChild(proArticle);
+        proLink.className = "projects__item";
+
+        document.querySelector(".pro .projects__list").appendChild(proLink);
+
+    } else if (project.category == "uni") {
+        const uniLogo = document.createElement("img");
+        uniLogo.className = "uni__img";
+        uniLogo.alt = project.logo_alt;
+        uniLogo.src = project.logo_url;
+
+        const uniName = document.createElement("h4");
+        uniName.className = "no-margin";
+        uniName.innerHTML = project.name;
+
+        const uniDates = document.createElement("p");
+        uniDates.className = "projects__dates";
+        uniDates.innerText = project.dates;
+
+        const uniTags = document.createElement("div");
+        uniTags.className = "tags";
+        for (var t=0; t < project.tags_short.length; t++) {
+            var tag = document.createElement("span");
+            tag.className = project.tags_colors_short[t];
+            tag.innerText = project.tags_short[t];
+            uniTags.appendChild(tag);
+        }
+
+        const uniInfo = document.createElement("div");
+        uniInfo.appendChild(uniName);
+        uniInfo.appendChild(uniDates);
+        uniInfo.appendChild(uniTags);
+
+
+        const uniArticle = document.createElement("article");
+        uniArticle.classList.add("projects__card", "uni__project");
+        uniArticle.appendChild(uniLogo);
+        uniArticle.appendChild(uniInfo);
+
+        const uniLink = document.createElement("a");
+        uniLink.href = "project.html";
+        uniLink.appendChild(uniArticle);
+        uniLink.className = "projects__item";
+
+        document.querySelector(".uni .projects__list").appendChild(uniLink);
+
+    } else if (project.category == "art") {
+
+        const artLogo = document.createElement("img");
+        artLogo.className = "art__img";
+        artLogo.alt = project.logo_alt;
+        artLogo.src = project.logo_url;
+
+        const artName = document.createElement("h4");
+        artName.className = "no-margin";
+        artName.innerHTML = project.name;
+
+        const artDates = document.createElement("p");
+        artDates.className = "projects__dates";
+        artDates.innerText = project.dates;
+
+        const artDesc = document.createElement("p");
+        artDesc.classList.add("art__desc", "no-margin");
+        artDesc.innerHTML = project.desc;
+
+        const artInfo = document.createElement("div");
+        artInfo.className = "projects__info";
+        artInfo.appendChild(artName);
+        artInfo.appendChild(artDates);
+        artInfo.appendChild(artDesc);
+
+        const artArticle = document.createElement("article");
+        artArticle.classList.add("projects__card", "art__project");
+        artArticle.appendChild(artLogo);
+        artArticle.appendChild(artInfo);
+
+        const artLink = document.createElement("a");
+        artLink.href = "project.html";
+        artLink.appendChild(artArticle);
+        artLink.className = "projects__item";
+
+        document.querySelector(".art .projects__list").appendChild(artLink);
     }
 }
 
@@ -14,7 +139,7 @@ function fetchData() {
         .then((data) => {
             projectsData = data;
             for (project of projectsData) {
-                //addProjectHTML(project);
+                addProjectHTML(project);
             }
             
         })
